d776f61b8f980098715071fecbc482ae784257a9
Minjie Huang <minjie.huang@tigerspike.com>
2022-09-06 10:13:51 +1000

    add extra prop to raw react component

--- 01-no-frills-react/src/App.js
+++ 01-no-frills-react/src/App.js
@@ -3,23 +3,31 @@ const Pet = (props) => {
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
     React.createElement("h2", {}, props.breed),
+    React.createElement("h2", {}, props.special),
   ]);
 };
 
-const App = () => {
+const App = (special) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
       name: "Luna",
       animal: "Dog",
       breed: "Havanese",
+      special: "special",
     }),
     React.createElement(Pet, {
       name: "Pepper",
       animal: "Bird",
       breed: "Cockatiel",
+      special: "prop",
     }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mix",
+      special: "prop",
+    }),
   ]);
 };
 
